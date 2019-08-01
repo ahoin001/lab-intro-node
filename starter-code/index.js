d958a23878bb343d0ed89ef66894b1d435dcab67
Alexander Hoinville <ahoin001@gmail.com>
2019-08-01 19:32:48 -0400

    Create error catcher for inddex search

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,35 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor(items, length) {
+
+    this.items = [];
+    this.length = 0;
+
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.length++;
+  }
+
+  get(pos) {
+
+    let searchedItem;
+
+    // if index greater than array or negative , throw an error
+    if (pos > this.items.length || pos < 0) {
+      throw new Error("EmptySortedList");
+    }
+    else {
+      searchedItem = this.items[pos];
+    }
+
+    return searchedItem;
+
+  }
+  max() { }
+  min() { }
+  average() { }
+  sum() { }
+};
+
+module.exports = SortedList;
